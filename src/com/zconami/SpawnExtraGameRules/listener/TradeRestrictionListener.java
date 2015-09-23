58864fea19a8dd9c1c1c66232c37f4285ff8a02d
Jonny Power <jonny@sunguru.co>
2015-09-23 23:23:42 +0100

    stupid

--- src/com/zconami/SpawnExtraGameRules/listener/TradeRestrictionListener.java
+++ src/com/zconami/SpawnExtraGameRules/listener/TradeRestrictionListener.java
@@ -72,7 +72,7 @@ public class TradeRestrictionListener implements Listener {
         }
 
         final List<String> lore = itemMeta.getLore();
-        if (lore.size() < 2 || lore == null) {
+        if (lore == null || lore.size() < 2) {
             return false;
         }
 
